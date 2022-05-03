78bdab3c9a0461feb7b551355c8bb3be420308ac
Esther Cho <xesthercho@gmail.com>
2022-05-03 12:30:28 -0700

    Added controllers for users and location
    Co-authored-by: Jestyn Apuya <jestynapuya@gmail.com>
    Co-authored-by: Marcellies Pettiford <marcellies@pettifords.xyz>

---
+++ src/Server/controllers/locationController.js
@@ -0,0 +1,31 @@
+import fetch from 'node-fetch'
+
+const locationController = {
+  getLocation(req, res, next) {
+    try {
+      
+      console.log('body: ', req.body)
+      const {city, state} = req.body
+      // const city = req.body.city
+      // const state = req.body.state
+
+      fetch(`https://api.yelp.com/v3/businesses/search?location=${city},${state}&term=lgbtq`, {
+        headers: {
+            'Authorization': 'Bearer H9XvL6dp0gjVn8NKCTG8d8041RWlw0EMX9bsX6dRptipWCpuzAaVTulqewlWlMvPfJG5LW28crhpWqDvc2v6dFaC0gY4U3iTFnaoWbovZJUMWian5ycQVCq941FwYnYx',
+        }
+      })
+      .then(data => data.json())
+      .then((data) => {
+        console.log('data: ', data)
+        res.locals.data = data.businesses
+        return next()
+      })
+      .catch(error => console.log('error in search bar get request: ', error))
+    }
+    catch (error) {
+      return next(error)
+    }
+  }
+}
+
+export default locationController
