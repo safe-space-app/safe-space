435f31386b545a0f1c04056d416a705fa808f985
Esther Cho <xesthercho@gmail.com>
2022-05-03 12:28:32 -0700

    Added route handling for users
    Co-authored-by: Jestyn Apuya <jestynapuya@gmail.com>
    Co-authored-by: Marcellies Pettiford <marcellies@pettifords.xyz>

--- src/Server/server.js
+++ src/Server/server.js
@@ -1,14 +1,30 @@
-import express from 'express'
-import cors from 'cors'
-import locationController from './locationController.js'
+import express from 'express';
+import cors from 'cors';
+import mongoose from 'mongoose';
+import locationController from './controllers/locationController.js';
+import userController from './controllers/userController.js';
 const app = express();
 const PORT = 3000;
+
+
+mongoose.connect('mongodb+srv://admin:squirtle123@cluster0.yt6wc.mongodb.net/Cluster0?authMechanism=DEFAULT');
+mongoose.connection.once('open', () => {
+  console.log('Connected to database');
+})
 
 app.use(cors({origin: 'http://localhost:8080',credentials: true,}));
 app.use(express.json());
 
-app.post('/getLocation', locationController.getLocation, (req, res, next) => {
+app.post('/getLocation', locationController.getLocation, (req, res) => {
   res.status(200).json(res.locals.data)
+})
+
+app.post('/createAccount', userController.createAccount, (req, res) => {
+  res.sendStatus(200)
+})
+
+app.post('/login', userController.login, (req, res) => {
+  res.status(200).json(res.locals.account);
 })
 
 app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
